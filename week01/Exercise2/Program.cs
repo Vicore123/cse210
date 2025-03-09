193763f557aa31a073ed854db25bb09e6cd685ca
Vicore123 <viniciuslimaalves123@gmail.com>
2025-03-09 17:32:13 -0300

    week1 activities

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -3,7 +3,35 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+    {   
+        Console.Write("what is your grade (in numbers)? ");
+        string grade = Console.ReadLine();
+        int gradeNumber = int.Parse(grade);
+        string gradeLetter = "";
+
+        if (gradeNumber >= 90) {
+            gradeLetter = "A";
+        }
+        else if (gradeNumber >= 80) {
+            gradeLetter = "B";
+        }
+        else if (gradeNumber >= 70) {
+            gradeLetter = "C";
+        }
+        else if (gradeNumber >= 60) {
+            gradeLetter = "D";
+        }
+        else {
+            gradeLetter = "F";
+        }
+
+        Console.WriteLine($"Your grade is {gradeLetter}");
+
+        if (gradeNumber >= 70) {
+            Console.WriteLine("You passed on this course");
+        }
+        else {
+            Console.WriteLine("You did not pass on this course");
+        }
     }
 }
